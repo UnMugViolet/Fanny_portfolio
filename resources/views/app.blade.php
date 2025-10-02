098c783917cae3ec3918b2db7fb4cc53bdb2ebd8
unmugviolet <jaguinpaul@gmail.com>
2025-10-02 16:34:25 +0200

    Added metatags to the homepage

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,10 +3,18 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{ config('app.name', 'Fanny Portfolio') }}</title>
+    <title>{{ 'Fanny Séraudie | Portfolio' }}</title>
     
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Meta -->
+    <meta name="title" content="Fanny Séraudie - Illustratrice et animatrice 2D">
+    <meta name="description" content="Découvrez le portfolio de Fanny Séraudie, illustratrice et animatrice 2D. Explorez ses projets créatifs et son univers artistique unique.">
+    <meta property="og:title" content="Fanny Séraudie - Illustratrice et animatrice 2D">
+    <meta property="og:description" content="Découvrez le portfolio de Fanny Séraudie, illustratrice et animatrice 2D. Explorez ses projets créatifs et son univers artistique unique.">
+    <meta property="og:image" content="{{ asset('img/logo-fanny-seraudie.webp') }}">
+    <meta name="robots" content="index, follow">
     
     <!-- Styles and Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
