1ba11a1370b34ec00f6c0db4a45689266075494f
unmugviolet <jaguinpaul@gmail.com>
2025-10-08 15:08:40 +0200

    Update title and meta legal pages

--- resources/views/legal_notice.blade.php
+++ resources/views/legal_notice.blade.php
@@ -3,17 +3,17 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Politique de confidentialité | Fanny Séraudie Portfolio</title>
+	<title>Mentions Legales | Fanny Séraudie Portfolio</title>
 	
 	<!-- CSRF Token -->
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 
 	<!-- SEO -->
-    <meta name="description" content="">
-    <meta property="og:title" content="">
-    <meta property="og:description" content="">
-    <meta property="og:image" content="">
-	<meta name="robots" content="">
+    <meta name="description" content="Mentions légales du site web Fanny Séraudie Portfolio.">
+    <meta property="og:title" content="Mentions Legales | Fanny Séraudie Portfolio">
+    <meta property="og:description" content="Mentions légales du site web Fanny Séraudie Portfolio.">
+    <meta property="og:image" content="{{ asset('img/logo-fanny-seraudie.webp') }}">
+	<meta name="robots" content="noindex, nofollow">
 
 	<!-- Styles and Scripts -->
 	@vite(['resources/css/app.css', 'resources/js/app.js'])
--- resources/views/privacy_policy.blade.php
+++ resources/views/privacy_policy.blade.php
@@ -9,11 +9,11 @@
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 
 	<!-- SEO -->
-    <meta name="description" content="">
-    <meta property="og:title" content="">
-    <meta property="og:description" content="">
-    <meta property="og:image" content="">
-	<meta name="robots" content="">
+    <meta name="description" content="Politique de confidentialité de Fanny Séraudie Portfolio.">
+    <meta property="og:title" content="Politique de confidentialité | Fanny Séraudie Portfolio">
+    <meta property="og:description" content="Politique de confidentialité de Fanny Séraudie Portfolio.">
+    <meta property="og:image" content="{{ asset('img/logo-fanny-seraudie.webp') }}">
+	<meta name="robots" content="noindex, nofollow">
 
 	<!-- Styles and Scripts -->
 	@vite(['resources/css/app.css', 'resources/js/app.js'])
