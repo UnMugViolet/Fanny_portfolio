d956ef645b9f538afe90b92fa8651ac7313cdd8f
UnMugViolet <jaguinpaul@gmail.com>
2025-10-29 20:09:58 +0100

    Update favicon on blade templates

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ 'Fanny Séraudie - Illustratrice et animatrice 2D | Portfolio' }}</title>
     
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon.ico') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon.ico') }}">
+    <link rel="apple-touch-icon" href="{{ asset('favicon.ico') }}">
+    
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
--- resources/views/legal_notice.blade.php
+++ resources/views/legal_notice.blade.php
@@ -8,6 +8,12 @@
 	<!-- CSRF Token -->
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 
+	<!-- Favicon -->
+	<link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+	<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon.ico') }}">
+	<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon.ico') }}">
+	<link rel="apple-touch-icon" href="{{ asset('favicon.ico') }}">
+
 	<!-- SEO -->
     <meta name="description" content="">
     <meta property="og:title" content="">
